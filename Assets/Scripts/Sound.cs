3a16912962fbd0f3378a5456e9991b3f0f2add76
alexey.anipchenko <anipchenko@mindwave.studio>
2020-11-22 20:55:04 +0200

    Add sounds descriptor controller

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+/// <summary>
+/// Sound descriptor
+/// </summary>
+[System.Serializable]
+public class Sound
+{
+    public string Name;
+
+    public AudioClip Clip;
+
+    [Range(0f, 1f)]
+    public float Volume = 1f;
+
+    [Range(-3f, 3f)]
+    public float Pitch = 1f;
+
+    public bool Loop = false;
+
+    /// <summary>
+    /// Visible in Classes but not in Inspector
+    /// </summary>
+    [HideInInspector]
+    public AudioSource Source;
+}
